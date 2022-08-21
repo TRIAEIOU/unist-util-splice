9c7848cb324f9592d60975e75e9e95c8068e16d7
TRIAEIOU <94647023+TRIAEIOU@users.noreply.github.com>
2022-08-21 19:56:39 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import {Node, Parent} from 'unist'
+
+/**
+ * Replaces `node` with replacement in `parents` children
+ * @param {Node} node to replace
+ * @param {Parent} parent of node
+ * @param {Node|Node[]} replacement for node
+ * @returns {Number} index of end of replacement or -1
+ */
+export function split(node, parent, replacement) {
+    if (!parent?.children.length) return -1
+    if (!Array.isArray(replacement)) replacement = [replacement]
+    const plen = parent.children.length
+    let i = 0
+    while (parent.children[i] !== node && i < plen) i++;
+    if (i === plen) return -1
+    parent.children.splice(i, 1, replacement)
+    return i + replacement.length
+}
